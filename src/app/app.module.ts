45565cde7397a944612af3ad57c54c8852aca9d3
DanielEleazarPrietoWilches <danielprietowilches@gmail.com>
2020-07-24 14:39:15 -0500

    'header'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WorkingComponent } from './components/working/working.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    WorkingComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
